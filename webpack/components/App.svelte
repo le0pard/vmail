dc674b9b2d6ea8cf587e9514045c9fc414387470
Alexey Vasiliev <leopard.not.a@gmail.com>
2021-11-11 00:52:23 +0200

    split on more views

--- webpack/components/App.svelte
+++ webpack/components/App.svelte
@@ -2,8 +2,10 @@
 	import {onDestroy} from 'svelte'
 	import {report, reportLoading, reportError} from 'stores/report'
 	import {splitState} from 'stores/split'
+	import EditorHeaderComponent from './EditorViewHeader'
 	import EditorViewComponent from './EditorView'
 	import SplitViewComponent from './SplitView'
+	import ReportHeaderComponent from './ReportViewHeader'
 	import ReportViewComponent from './ReportView'
 
 	export let parserFunction
@@ -34,36 +36,12 @@
 		position: relative;
 	}
 
-	.parser-editor-header {
-		background-color: var(--headColor);
-		flex-shrink: 0;
-		overflow: hidden;
-    white-space: nowrap;
-		box-shadow: 0 2px 2px rgb(0 0 0 / 3%), 0 1px 0 rgb(0 0 0 / 3%);
-		padding: 0.3rem 0;
-	}
-
-	.parser-editor-header {
-		flex-shrink: 0;
-		overflow: hidden;
-    white-space: nowrap;
-	}
-
 	.parser-report {
 		height: 100%;
 		flex: 1;
 		position: relative;
 		display: flex;
 		flex-direction: column;
-	}
-
-	.parser-report-header {
-		background-color: var(--headColor);
-		flex-shrink: 0;
-		overflow: hidden;
-    white-space: nowrap;
-		box-shadow: 0 2px 2px rgb(0 0 0 / 3%), 0 1px 0 rgb(0 0 0 / 3%);
-		padding: 0.3rem 0;
 	}
 
 	.parser-editor-hidden, .parser-report-hidden {
@@ -73,18 +51,12 @@
 
 <div class="parser-view">
 	<div class="parser-editor" class:parser-editor-hidden="{$splitState.visible === 'right'}">
-		<div class="parser-editor-header">
-			<a href="/">Home</a>
-			<a href="/faq.html">FAQ</a>
-		</div>
+		<EditorHeaderComponent />
 		<EditorViewComponent parserFunction={parserFunction} />
 	</div>
 	<SplitViewComponent />
 	<div class="parser-report"  class:parser-report-hidden="{$splitState.visible === 'left'}">
-		<div class="parser-report-header">
-			<a href="/">Home</a>
-			<a href="/faq.html">FAQ</a>
-		</div>
+		<ReportHeaderComponent />
 		<ReportViewComponent />
 	</div>
 </div>
---
+++ webpack/components/ReportViewHeader.svelte
@@ -0,0 +1,15 @@
+<style>
+  .report-header {
+		background-color: var(--headColor);
+		flex-shrink: 0;
+		overflow: hidden;
+    white-space: nowrap;
+		box-shadow: 0 2px 2px rgb(0 0 0 / 3%), 0 1px 0 rgb(0 0 0 / 3%);
+		padding: 0.3rem 0;
+	}
+</style>
+
+<div class="report-header">
+  <a href="/">Home</a>
+  <a href="/faq.html">FAQ</a>
+</div>
