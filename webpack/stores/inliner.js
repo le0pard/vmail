7905faba7978613d37055e779e4b63daf18e6cba
Alexey Vasiliev <leopard.not.a@gmail.com>
2021-11-27 15:19:05 +0200

    finish inline css functionality

---
+++ webpack/stores/inliner.js
@@ -0,0 +1,14 @@
+import {writable} from 'svelte/store'
+
+const createBasicStore = (initialVal = null) => {
+  const {subscribe, set} = writable(initialVal)
+
+  return {
+    subscribe,
+    set,
+    reset: () => set(initialVal)
+  }
+}
+
+export const inlinerLoading = createBasicStore(false)
+export const inlinerError = createBasicStore(null)
