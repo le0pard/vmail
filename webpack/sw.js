77a3cd1ad950c7f14c6abe8e78eaf815f330f900
Alexey Vasiliev <leopard.not.a@gmail.com>
2021-12-03 12:22:06 +0200

    more settings in wasm

--- webpack/sw.js
+++ webpack/sw.js
@@ -2,6 +2,7 @@ import {clientsClaim} from 'workbox-core'
 import {precacheAndRoute} from 'workbox-precaching'
 import {registerRoute} from 'workbox-routing'
 import {NetworkFirst} from 'workbox-strategies'
+import {BackgroundSyncPlugin} from 'workbox-background-sync'
 import {ExpirationPlugin} from 'workbox-expiration'
 import {cleanupOutdatedCaches} from 'workbox-precaching/cleanupOutdatedCaches'
 
@@ -30,15 +31,24 @@ cleanupOutdatedCaches()
 
 // wasm cache router
 registerRoute(
-  new RegExp('/.*\\.wasm$', 'i'),
+  new RegExp('\\.wasm$', 'i'),
   new NetworkFirst({
     cacheName: 'wasm-modules',
-    networkTimeoutSeconds: 20,
+    networkTimeoutSeconds: 5,
+    matchOptions: {
+      ignoreSearch: true
+    },
     plugins: [
       new ExpirationPlugin({
-        maxEntries: 5,
+        maxEntries: 10,
         maxAgeSeconds: 90 * 24 * 60 * 60, // 90 days
+        matchOptions: {
+          ignoreSearch: true
+        },
         purgeOnQuotaError: true
+      }),
+      new BackgroundSyncPlugin('wasmQueue', {
+        maxRetentionTime: 24 * 60 // Retry for max of 24 Hours (specified in minutes)
       })
     ]
   })
